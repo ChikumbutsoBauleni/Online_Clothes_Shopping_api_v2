27af80e4ebc38b1ce86df52f03bc13f857ef7707
bsc-inf-16-19 <bsc-inf-16-19@unima.ac.mw>
2023-04-28 23:27:14 +0200

    databse commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CatsModule],
+
+
+  imports: [CatsModule,
+  TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'cats',
+    autoLoadEntities:true,
+    synchronize: true,
+  }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/cats/entities/cat.entity.ts
+++ src/cats/entities/cat.entity.ts
@@ -1,1 +1,16 @@
-export class Cat {}
+import { ApiProperty } from "@nestjs/swagger/dist";
+export class Cat {
+
+        @ApiProperty()
+        id: number;
+      
+        @ApiProperty()
+        name: string;
+      
+        @ApiProperty()
+        age: number;
+      
+        @ApiProperty()
+        breed: string;
+      
+}
